0d16dbf66d7c1efc8371130e0c809f18c8229fa7
Michael Cheng <michael.cheng@flatironschool.com>
2018-12-20 15:34:07 -0500

    boom, ar

--- 09-intro-to-active-record/app/models/ticket.rb
+++ 09-intro-to-active-record/app/models/ticket.rb
@@ -1,2 +1,15 @@
 class Ticket < ActiveRecord::Base
+  # instead of attr_reader :airline, :passenger
+  belongs_to :airline
+  belongs_to :passenger # MACROS, attr_
+
+  # baren wasteland!!!
+
+  # def airline
+  #   Airline.find(self.airline_id)
+  # end
+
+  # def passenger
+  #   Passenger.find(self.passenger_id)
+  # end
 end
