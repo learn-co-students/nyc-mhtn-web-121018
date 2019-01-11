7da995b7c3b07369f86e82ce354fe299001e83a9
Michael Cheng <michael.cheng@flatironschool.com>
2019-01-11 10:43:00 -0500

    comments

--- 19-intro-to-rails/life/db/migrate/20190111150326_create_children.rb
+++ 19-intro-to-rails/life/db/migrate/20190111150326_create_children.rb
@@ -5,7 +5,7 @@ class CreateChildren < ActiveRecord::Migration[5.2]
       t.integer :ssn
       t.datetime :birthday
 
-      t.timestamps # for both created and updated timestamps. whatup????
+      t.timestamps #  for both created and updated timestamps. whatup????
     end
   end
 end
