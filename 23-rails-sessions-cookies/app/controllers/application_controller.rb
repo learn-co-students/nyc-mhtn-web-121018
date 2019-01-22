34e342212ec0c13a969905b1cee487418eb0c887
Michael Cheng <michael.cheng@flatironschool.com>
2019-01-22 12:22:12 -0500

    cookie ingredients

--- 23-rails-sessions-cookies/app/controllers/application_controller.rb
+++ 23-rails-sessions-cookies/app/controllers/application_controller.rb
@@ -1,13 +1,2 @@
 class ApplicationController < ActionController::Base
-  def cart
-    session[:cart] ||= []
-  end
-
-  def add_nacho_to_cart(nacho_id)
-    cart << nacho_id
-  end
-
-  def get_items_from_cart
-    @cart_items = Nacho.find(cart)
-  end
 end
--- 23-rails-sessions-cookies/config/routes.rb
+++ 23-rails-sessions-cookies/config/routes.rb
@@ -1,4 +1,3 @@
 Rails.application.routes.draw do
   resources :nachos, only: [:index, :show]
-  patch '/cart', to: 'cart#update', as: 'add_to_cart'
 end
