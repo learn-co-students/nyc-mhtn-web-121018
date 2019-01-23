87f3babebd8a72feb03537d3e6f4a4cdb212e2ed
Michael Cheng <michael.cheng@flatironschool.com>
2019-01-23 10:33:11 -0500

    auth starter

---
+++ 25-rails-auth/app/controllers/users_controller.rb
@@ -0,0 +1,13 @@
+class UsersController < ApplicationController
+  def show
+    @user = User.find(params[:id])
+  end
+
+  def new
+    @user = User.new
+  end
+
+  def create
+    # TODO: signup
+  end
+end
---
+++ 25-rails-auth/db/migrate/20181002134914_create_users.rb
@@ -0,0 +1,10 @@
+class CreateUsers < ActiveRecord::Migration[5.2]
+  def change
+    create_table :users do |t|
+      t.string :username
+      t.string :avatar, default: 'https://media.gq.com/photos/59dfc6d9d61cb80476584e18/3:2/w_800/guy-fieiri-flame.jpg'
+
+      t.timestamps
+    end
+  end
+end
