d5dc323f0dcc0d0256f4aa7d2dc9a117fc8b96e9
Michael Cheng <michael.cheng@flatironschool.com>
2019-01-29 09:33:31 -0500

    fixed starter

--- 28-dom-intro/src/index.js
+++ 28-dom-intro/src/index.js
@@ -1,22 +1,11 @@
-console.log('%c Hello, World!', 'color: orange')
 
-// DO NOT DO THIS:
+// Deliverables:
 
-document.getElementById('outer-node').children[0].children[0].children[0]
+// 1. Practice grabbing something from the DOM
 
+// 2. Practice making a new element
 
-const outerNode = document.querySelector('#outer-node')
-const internetMemeHeader = document.querySelector('#outer-node h1')
+// 3. Practice adding elements to the DOM
 
-// find the div with an id of container
-const container = document.querySelector('#container')
-
-// need to iterate over array of imgs
-dankMemes.forEach(function(memeUrlString) {
-  // create a new img tag
-  const newImgTag = document.createElement('img')
-  //set the src of the new img tag to EACH img in the array
-  newImgTag.src = memeUrlString
-  // append the img tag to the container div
-  container.appendChild(newImgTag)
-})
+// 4. Bringing it all together:
+//      Put some dank memes on the page!
